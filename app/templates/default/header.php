5d078a01197a4c08032f10b8353dca38604efe16
jordandevolder <jordan_devolder@ens.univ-artois.fr>
2016-01-27 18:12:00 +0100

    validation

---
+++ app/templates/default/header.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Sample layout
+ */
+
+use Helpers\Assets;
+use Helpers\Url;
+use Helpers\Hooks;
+
+//initialise hooks
+$hooks = Hooks::get();
+?>
+<!DOCTYPE html>
+<html lang="<?php echo LANGUAGE_CODE; ?>">
+<head>
+
+	<!-- Site meta -->
+	<meta charset="utf-8">
+	<?php
+	//hook for plugging in meta tags
+	$hooks->run('meta');
+	?>
+	<title><?php echo $data['title'].' - '.SITETITLE; //SITETITLE defined in app/Core/Config.php ?></title>
+
+
+    <header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+        <div class="container">
+            <div class="navbar-header">
+                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="./" class="navbar-brand">Projet Semestre 4</a>
+            </div>
+            <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Règles</a>
+                    </li>
+                    <li>
+                        <a href="#">Tableau des scores</a>
+                    </li>
+                    <li>
+                        <a href="#">Connexion</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+	<!-- CSS -->
+	<?php
+	Assets::css(array(
+		'//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+		Url::templatePath() . 'css/style.css',
+	));
+
+	//hook for plugging in css
+	$hooks->run('css');
+	?>
+
+</head>
+
+
+<body>
+<?php
+//hook for running code after body tag
+$hooks->run('afterBody');
+?>
+
+<div class="container">
