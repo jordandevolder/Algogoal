d81576782843ba244cb5681e3c813f5b3f086991
LenweSeregon <serf.nicolas@gmail.com>
2016-02-25 16:21:15 +0100

    Optimisation image

--- javascript/script/representation.js
+++ javascript/script/representation.js
@@ -1,72 +1,11 @@
 canvas = null;
 context = null;
+grid = null;
+imageTab = {};
 
 
 window.onload = function()
 {
-    var avancer = document.getElementById('avancer');
-
-    avancer.addEventListener('click', function() {
-        player.move();
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        grid.drawMap();
-        grid.drawGrid();
-        grid.drawPlayer();
-    });
-
-    var pousser = document.getElementById('pousser');
-
-        pousser.addEventListener('click', function() {
-        player.push();
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        grid.drawMap();
-        grid.drawGrid();
-        grid.drawPlayer();
-    });
-
-    var rotateL = document.getElementById('rotateL');
-
-        rotateL.addEventListener('click', function() {
-        player.leftRotate();;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        grid.drawMap();
-        grid.drawGrid();
-        grid.drawPlayer();
-    });
-
-    var rotateR = document.getElementById('rotateR');
-
-        rotateR.addEventListener('click', function() {
-        player.rightRotate();
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        grid.drawMap();
-        grid.drawGrid();
-        grid.drawPlayer();
-    });
-
-    var collect = document.getElementById('collect');
-
-        collect.addEventListener('click', function() {
-        player.collect();
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        grid.drawMap();
-        grid.drawGrid();
-        grid.drawPlayer();
-    });
-
-    var jump = document.getElementById('jump');
-
-        jump.addEventListener('click', function() {
-        player.jump();
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        grid.drawMap();
-        grid.drawGrid();
-        grid.drawPlayer();
-    });
-
-
-
-
     canvas = document.getElementById('mon_canvas');
     if(!canvas)
     {
@@ -80,32 +19,184 @@ window.onload = function()
         return;
     }
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    
-    var grid = new GridGraphic();
+    grid = new GridGraphic();
 
+    var avancer = document.getElementById('avancer');
+    avancer.addEventListener('click', function() {
+        player.move();
+        draw();
+    });
+
+    var pousser = document.getElementById('pousser');
+    pousser.addEventListener('click', function() {
+        player.push();
+        draw();
+    });
+
+    var rotateL = document.getElementById('rotateL');
+    rotateL.addEventListener('click', function() {
+        player.leftRotate();;
+        draw();
+    });
+
+    var rotateR = document.getElementById('rotateR');
+    rotateR.addEventListener('click', function() {
+        player.rightRotate();
+        draw();
+    });
+
+    var collect = document.getElementById('collect');
+    collect.addEventListener('click', function() {
+        player.collect();
+        draw();
+    });
+
+    var jump = document.getElementById('jump');
+    jump.addEventListener('click', function() {
+        player.jump();
+        draw();
+    });
+
+    createImageTab();
+    draw();
+};
+
+function draw(){
+    context.clearRect(0,0,canvas.width, canvas.height);
+    context.beginPath();
     grid.drawMap();
     grid.drawGrid();
     grid.drawPlayer();
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    player.showInformations();
-
-    grid.drawMap();
-    grid.drawGrid();
-    grid.drawPlayer();
-    
     context.stroke();
     context.closePath();
 }
+
+function createImageTab(){
+
+
+    var arrayString =   ["grass","weapon","gold","player","monster","arrow", "road",
+                        "stone","goal","obstacle","water","obstacleUnmovable","waterWalkable"];
+
+    for(var i = 0; i < arrayString.length; i++){
+        imageTab[arrayString[i]] = new Image();
+        switch(i)
+        {
+            case 0:
+                imageTab[arrayString[i]].src = "../projet/image/grass.jpg";
+                break;
+            case 1:
+                imageTab[arrayString[i]].src = "../projet/image/Bow.png";
+                break;
+            case 2:
+                imageTab[arrayString[i]].src = "../projet/image/Gold.png";
+                break;
+            case 3:
+                imageTab[arrayString[i]].src = "../projet/image/Player.png";
+                break;
+            case 4:
+                imageTab[arrayString[i]].src = "../projet/image/Monster.png";
+                break;
+            case 5:
+                imageTab[arrayString[i]].src = "../projet/image/Arrow.png";
+                break;
+            case 6:
+                imageTab[arrayString[i]].src = "../projet/image/DirtRoad.jpg";
+                break;
+            case 7:
+                imageTab[arrayString[i]].src = "../projet/image/Stone.jpg";
+                break;
+            case 8:
+                imageTab[arrayString[i]].src = "../projet/image/Goal.jpg";
+                break;
+            case 9:
+                imageTab[arrayString[i]].src = "../projet/image/Obstacle.png";
+                break;
+            case 10:
+                imageTab[arrayString[i]].src = "../projet/image/Water.jpg";
+                break;
+            case 11:
+                imageTab[arrayString[i]].src = "../projet/image/obstacleUnmovable.png";
+                break;
+            case 12:
+                imageTab[arrayString[i]].src = "../projet/image/WaterWalkable.png";
+                break;
+        }
+    }
+}
+
 
 function FactoryImage(){
 
 }
 
+
 FactoryImage.prototype.createImageFrom = function(typeTile, posX, posY){
-    image = new Image();
+    switch(typeTile) {
+        case 0:
+            return imageTab["grass"];
+            break;
+        case 1:
+            if (!hasCollectWeapon && map.map[posY][posX].typeId == EntityType.WEAPON) {
+                return imageTab["weapon"];
+            }
+            else {
+                return imageTab["road"];
+            }
+            break;
+        case 2:
+            if (!hasCollectGold && map.map[posY][posX].typeId == EntityType.GOLD) {
+                return imageTab["gold"];
+            }
+            else {
+                return imageTab["road"];
+            }
+            break;
+        case 3:
+            return imageTab["player"];
+            break;
+        case 4:
+            if (!hasKillMonster && map.map[posY][posX].typeId == EntityType.MONSTER) {
+                return imageTab["monster"];
+            }
+            else {
+                return imageTab["road"];
+            }
+            break;
+        case 5:
+            if (!hasCollectArrow && map.map[posY][posX].typeId == EntityType.AMMO) {
+                return imageTab["arrow"];
+            }
+            else {
+                return imageTab["road"];
+            }
+            break;
+        case 6:
+            return imageTab["road"];
+            break;
+        case 7:
+            return imageTab["stone"];
+            break;
+        case 8:
+            return imageTab["goal"];
+            break;
+        case 9:
+            return imageTab["obstacle"];
+            break;
+        case 10:
+            return imageTab["water"];
+            break;
+        case 11:
+            return imageTab["obstacleUnmovable"];
+            break;
+        case 12:
+            return imageTab["waterWalkable"];
+            break;
+    }
+};
+
+/*
+FactoryImage.prototype.createImageFrom = function(typeTile, posX, posY){
+    var image = new Image();
     switch(typeTile)
     {
         case 0:
@@ -116,7 +207,7 @@ FactoryImage.prototype.createImageFrom = function(typeTile, posX, posY){
                 image.src = "../projet/image/Bow.png";
             }
             else{
-                image.src = "../projet/image/DirtRoad.jpg";
+                image.src =  "../projet/image/DirtRoad.jpg";
             }
             break;
         case 2:
@@ -170,6 +261,7 @@ FactoryImage.prototype.createImageFrom = function(typeTile, posX, posY){
     }
     return image;
 };
+*/
 
 factoryImage = new FactoryImage();
 
@@ -182,7 +274,6 @@ function GridGraphic(){
 }
 
 GridGraphic.prototype.drawGrid = function(){
-    context.beginPath();
 
     //Drawing line grid
     for(var i = 0 ; i <= this.nbTileX; i++){
@@ -195,22 +286,24 @@ GridGraphic.prototype.drawGrid = function(){
         context.moveTo(i*this.xPerTile,0);
         context.lineTo(i*this.xPerTile,this.yPerTile*this.nbTileY);
     }
-}
+};
+
 
 GridGraphic.prototype.drawMap = function(){
 
+    console.log(imageTab);
     //Building of basic Tiles
     for(var i = 0; i < this.nbTileX; i++){
         for(var j = 0; j < this.nbTileY; j++){
-            var image = factoryImage.createImageFrom(map.map[j][i].typeId, i , j)
+            var image = factoryImage.createImageFrom(map.map[j][i].typeId, i , j);
             context.drawImage(image,i*this.xPerTile, j*this.yPerTile);
         }
     }
-}
+};
 
 GridGraphic.prototype.drawPlayer = function(){
     var imagePlayer = factoryImage.createImageFrom(EntityType.PLAYER,0,0);
     context.drawImage(imagePlayer, player.y*this.xPerTile, player.x*this.yPerTile);
-}
+};
 
 
