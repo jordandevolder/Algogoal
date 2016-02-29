42098c2145ef094d21007df083d09ff0b1ca7115
Jordan <contact@jordandevolder.fr>
2016-02-29 21:43:17 +0100

    coloration syntaxique hihi

--- app/views/connexion/connexion.php
+++ app/views/connexion/connexion.php
@@ -18,7 +18,7 @@ use Helpers\Form;
 Connexion à l'espace membre :<br />
 <form action="laconnexion" method="post">
 Login : <input type="text" name="login" value="<?php if (isset($_POST['login'])) echo htmlentities(trim($_POST['login'])); ?>"><br />
-Mot de passe : <input type="password" name="pass" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>"><br />
+Mot de passe : <input type="password" name="password" value="<?php if (isset($_POST['pass'])) echo htmlentities(trim($_POST['pass'])); ?>"><br />
 <input type="submit" name="connexion" value="Connexion">
 </form>
 <a href="/projet/inscription/">Vous inscrire</a>
