95149b88036acd852b1fb2c78d82af609fc4caad
Quentin <chyrro@outlook.fr>
2016-03-03 18:57:09 +0100

    debut creation vue regle

--- app/views/regles/regles.php
+++ app/views/regles/regles.php
@@ -10,6 +10,137 @@
 	<h1><?php echo $data['title'] ?></h1>
 
 </div>
+<div class="container">
+	<!-- DÉBUT DES INSTRUCTIONS  -->
+	<div class="row">
+		<div class="col-lg-3"></div>
+		<div class="col-lg-6"><h1 id="titreRegleCentre">Instructions</h1></div>
+		<div class="col-lg-3"></div>
+	</div>
 
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">Move</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+		<p> Bonjour</p>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">Push</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">Rotate</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">Collect</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">Jump</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<!-- DÉBUT DES CONTROLES  -->
+	<div class="row">
+		<div class="col-lg-3"></div>
+		<div class="col-lg-6"><h1 id="titreRegleCentre">Control</h1></div>
+		<div class="col-lg-3"></div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">While</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">If</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">Break</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<!-- DÉBUT DES CONTROLES  -->
+	<div class="row">
+		<div class="col-lg-3"></div>
+		<div class="col-lg-6"><h1 id="titreRegleCentre">Condition</h1></div>
+		<div class="col-lg-3"></div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">&&</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">||</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">!</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4"><h1 id="sousTitreRegleCentre">can{Action}</h1></div>
+		<div class="col-lg-4"></div>
+	</div>
+	<div class"row">
+	<p> Bonjour</p>
+	</div>
+
+</div>
 <?php
-echo $data['regles'];
+//expliquer toutes les commandes
+//expliquer le but du jeu
