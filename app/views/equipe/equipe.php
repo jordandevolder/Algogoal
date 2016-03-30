34fb2262602e5abd43d02496577c3c5c5db90993
Quentin Sauvage <quentin_sauvage@ens.univ-artois.fr>
2016-03-30 22:00:02 +0200

    calendrier des pompiers 2016

--- app/views/equipe/equipe.php
+++ app/views/equipe/equipe.php
@@ -25,7 +25,7 @@
         <div class="col-lg-5"><h1 id="sousTitreCentre">Christopher Hautekeur</h1>
             <div class="row">
                 <div class="col-lg-6">
-                    <img id="photo" src="image/jordan.jpg" class="img-responsive img-rounded">
+                    <img id="photo" src="image/christopher.jpg" class="img-responsive img-rounded">
                 </div>
                 <div class="col-lg-6">
                     <p> Christopher s'est occupé de la manière de faire le lien entre la partie moteur et le rendu graphique : Il a mis en place l'aspect graphique du jeu et sa gestion.</p>
@@ -36,7 +36,7 @@
         <div class="col-lg-5"><h1 id="sousTitreCentre">Nicolas Serf</h1>
             <div class="row">
                 <div class="col-lg-6">
-                    <img id="photo" src="image/jordan.jpg" class="img-responsive img-rounded">
+                    <img id="photo" src="image/nicolas.jpg" class="img-responsive img-rounded">
                 </div>
                 <div class="col-lg-6">
                     <p>Nicolas a conçu le jeu du point de vue moteur. C'est lui qui est à l'origine du bon fonctionnement des commandes qui se cache derrière les boutons tels que move et while. Il a également géré le canvas du jeu.</p>
@@ -69,7 +69,7 @@
         <div class="col-lg-5"><h1 id="sousTitreCentre">Quentin Sauvage</h1>
             <div class="row">
                 <div class="col-lg-6">
-                    <img id="photo" src="image/jordan.jpg" class="img-responsive img-rounded">
+                    <img id="photo" src="image/quentin.jpg" class="img-responsive img-rounded">
                 </div>
                 <div class="col-lg-6">
                     <p>Quentin a été chargé de la partie design du site qui avait pour but d'être le plus intuitif possible. Il a aussi mis en place la déconnexion de l'utilisateur et le tutoriel.</p>
